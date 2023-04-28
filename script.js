35e48e134c67f1c26ba0662c7444ab7d91133c01
Bernardo Azeredo <92824216+bernard0o@users.noreply.github.com>
2023-04-28 12:18:24 +0100

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const plane = document.getElementById("plane");
 const gameBoard = document.getElementById("game-board");
 let position = 50;
 
+// Moviment
 document.addEventListener("keypress", function(keyboard){
     if (keyboard.key == "d"){
         if (position < 95){
@@ -16,6 +17,26 @@ document.addEventListener("keypress", function(keyboard){
     }
 })
 
+// Enemies
+
+setInterval(function(){
+    let random = Math.floor(Math.random() * 98);
+    let top = 0;
+    let div = document.createElement("div");
+    div.className = "enemy";
+    gameBoard.appendChild(div);
+    div.style.left = `${random}%`;
+    setInterval(function(){
+        top += 10;
+        if (top >= 500){ // Game over
+            
+        }
+        div.style.top = `${top}px`
+    }, 200)
+}, 2000);
+
+
+// Lasers
 setInterval(function(){
     let bottom = 70;
     let div = document.createElement("div");
@@ -30,3 +51,4 @@ setInterval(function(){
         div.style.bottom = `${bottom}px`
     }, 50)
 }, 400);
+
