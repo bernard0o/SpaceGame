e82dac3b7946f1f05bbbb569e7588503bf0808c5
Bernardo Carvalho De Azeredo <2222095@alunos.escoladigital.com>
2023-05-08 11:25:41 +0100

    .

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 const plane = document.getElementById("plane");
 const gameBoard = document.getElementById("game-board");
 let position = 50;
+let moveRight = 0;
+let moveLeft = 0;
+let lasersPosition = [];
+let enemiesPosition = [];
 
-// Moviment
+// Main character moviment
 document.addEventListener("keypress", function(keyboard){
-    if (keyboard.key == "d"){
-        if (position < 95){
-            position += 1;
-        }
-        plane.style.left = `${position}%`;
-    } else if(keyboard.key == "a"){
-        if (position > 5){
-            position -= 1;
-        }
+    if (keyboard.key == "d" || keyboard.key == "ArrowRight"){
+        clearInterval(moveLeft);
+        clearInterval(moveRight);
+            moveRight = setInterval(function(){
+                if (position < 94){
+                    position += 3;
+                    plane.style.left = `${position}%`;
+                }
+            }, 100);
+    } else if(keyboard.key == "a" || keyboard.key == "ArrowLeft"){
+        clearInterval(moveRight);
+        clearInterval(moveLeft);
+            moveLeft = setInterval(function(){
+                if (position > 6){
+                    position -= 3;
+                    plane.style.left = `${position}%`;
+                }
+            }, 100);
         plane.style.left = `${position}%`;
     }
 })
 
 // Enemies
-
 setInterval(function(){
-    let random = Math.floor(Math.random() * 98);
-    let top = 0;
+
+    //Creating enemy div
+    let random = Math.floor(Math.random() * 90);
+    let top = -5;
     let div = document.createElement("div");
     div.className = "enemy";
     gameBoard.appendChild(div);
     div.style.left = `${random}%`;
+
+    //Loop to go down
     setInterval(function(){
         top += 10;
-        if (top >= 500){ // Game over
-            
+
+        // If enemy arrives bottom
+        if (top >= 500){
+            div.style.backgroundImage = "url(explosion.gif)";
+            div.style.width = "50px"
+            setTimeout(function(){
+                div.remove();
+            }, 500)
         }
         div.style.top = `${top}px`
     }, 200)
@@ -38,17 +60,42 @@ setInterval(function(){
 
 // Lasers
 setInterval(function(){
+
+    //Creating laser div
     let bottom = 70;
     let div = document.createElement("div");
     div.className = "laser";
     gameBoard.appendChild(div);
     div.style.left = `${position}%`;
-    setInterval(function(){
+
+    //Loop to go up
+    let laserMoviment = setInterval(function(){
         bottom += 10;
+        //When laser arrives top
         if (bottom >= 580){
             div.remove();
+            clearInterval(laserMoviment);
+            laserMoviment = 0;
         }
         div.style.bottom = `${bottom}px`
     }, 50)
 }, 400);
 
+//Detect if laser touches any enemy
+let detect = setInterval(function(){
+    lasersPosition = [];
+    const gameBoardChilds = gameBoard.children;
+    for (let i = 0; i < gameBoardChilds.length; i++){
+        if (gameBoardChilds[i].className == "laser"){
+            lasersPosition.push([gameBoardChilds[i].getBoundingClientRect().bottom, gameBoardChilds[i].getBoundingClientRect().left]);
+        }
+        else if (gameBoardChilds[i].className == "enemy"){
+            enemiesPosition.push([gameBoardChilds[i].getBoundingClientRect().bottom, gameBoardChilds[i].getBoundingClientRect().left]);
+        }
+    }
+    for (let i2=0; i2 < lasersPosition.length; i2++){
+        if (enemiesPosition.includes(lasersPosition[i2])){
+            lasersPosition[i2].remove();
+        }
+    }
+}, 50);
